f1b595984c04b3e26a0f074476b4c8ba9d687932
suha1028 <77815796+suha1028@users.noreply.github.com>
2021-03-08 15:51:30 -0500

    loading environment variables & dynamic url

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,9 +1,11 @@
 import requests 
+from dotenv import load_dotenv
 
 import json
 import csv 
 import os
 
+load_dotenv()
 
 def to_usd(my_price):
     """
@@ -15,8 +17,9 @@ def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
 #info inputs 
-
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
+api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+symbol = "MSFT"
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
 
 response = requests.get(request_url)
 
@@ -65,17 +68,8 @@ with open(csv_file_path, "w") as csv_file:
         "volume": daily_prices["5. volume"]
     })
 
-    writer.writerow({
-        "timestamp": "TODO",
-        "open": "TODO", 
-        "high": "TODO", 
-        "low": "TODO", 
-        "close": "TODO", 
-        "volume": "TODO"
-    })
-
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print(f"SELECTED SYMBOL: {symbol}")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
